0f35834ef92b0632a304ead54c3411dd5c34484d
Sharon <sharonelizalex@gmail.com>
2021-06-01 17:26:45 +1000

    added route folder structure

--- models/index.js
+++ models/index.js
@@ -1,10 +1,45 @@
-const Category = require('./Category');
 
-//Products  belong to Category
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+//Products  belong to Category      
+
+
+// Category - Product is One - Many
+
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
+
 
 // Categories has Many Products.
 
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+})
 
+
+//Product - Tags - Many to Many
+
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: 'tag_id'
+})
 //Products can have tags. Product belongs to Tags
 
+
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: 'product_id'
+})
 //Tags have many products linked to it.
+
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+  };
