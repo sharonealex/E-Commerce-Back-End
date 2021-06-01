b3bbb06bd4bc4ec2713eeaa62735f031982c2ad8
Sharon <sharonelizalex@gmail.com>
2021-06-01 16:47:04 +1000

    outline associations

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const Category = require('./Category');
+
+//Products  belong to Category
+
+// Categories has Many Products.
+
+
+//Products can have tags. Product belongs to Tags
+
+//Tags have many products linked to it.
