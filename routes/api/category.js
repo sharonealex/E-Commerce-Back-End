228646f0ae0a46e324697e2d75bac79cafa8773b
Sharon <sharonelizalex@gmail.com>
2021-06-13 14:54:00 +1000

    fixed error for sql join

--- routes/api/category.js
+++ routes/api/category.js
@@ -4,16 +4,22 @@ const { Category, Product } = require('../../models');
 // api endpoints for category
 
 //fetch all categories and the products within them.
-router.get('/', (request, response)=>{
-
-    Category.findAll()
-    .then((category)=>{
-response.json(category)
-    })
-    .catch((err)=>{
-        response.status(500).json(err)
-    })
-})
+router.get('/', (request, response) => {
+    Category.findAll(
+        {
+          include: {
+            model: Product,
+            attributes: ['product_name']
+          }
+        }
+      )
+        .then(categoryData => response.json(categoryData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
+    
 
 
 module.exports = router;
