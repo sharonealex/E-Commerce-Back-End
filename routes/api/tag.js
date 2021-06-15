902625dc4d0901bc60bc28fd26c59f4848607e8d
Sharon <sharonelizalex@gmail.com>
2021-06-15 17:04:48 +1000

    routes for tags table

--- routes/api/tag.js
+++ routes/api/tag.js
@@ -1,6 +1,18 @@
 const router = require("express").Router();
 const { Tag, Product, ProductTag } = require("../../models");
 
+//create
+router.post("/", (req, res) => {
+    Tag.create({
+      tag_name: req.body.tag_name,
+    })
+      .then((tag) => res.json(tag))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 //get all tags and associated products m:n
 
 router.get("/", (req, res) => {
@@ -36,5 +48,31 @@ router.get("/:id", (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.put("/:id", (req, res) => {
+   
+    Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+      .then((tag) => {
+        if (!tag) {
+          res.status(404).json({ message: "No Tag found with provided ID." });
+          return;
+        }
+        res.json(tag);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
 
 module.exports = router;
