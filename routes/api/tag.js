12372013c2bd71aa128f37f4891147d400004660
Sharon <sharonelizalex@gmail.com>
2021-06-15 17:00:00 +1000

    get tag by Id

--- routes/api/tag.js
+++ routes/api/tag.js
@@ -4,7 +4,6 @@ const { Tag, Product, ProductTag } = require("../../models");
 //get all tags and associated products m:n
 
 router.get("/", (req, res) => {
-
   Tag.findAll({
     include: [
       {
@@ -19,5 +18,23 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get("/:id", (req, res) => {
+    Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Product
+        },
+      ],
+    })
+      .then((tag) => res.json(tag))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
